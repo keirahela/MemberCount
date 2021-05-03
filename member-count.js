9a431bf77c6ba690d4db59bfeb4d04cf5cc052a5
keirahela <66428643+keirahela@users.noreply.github.com>
2021-05-03 16:46:44 +0200

    Update member-count.js

--- member-count.js
+++ member-count.js
@@ -3,7 +3,7 @@ module.exports = async (client) =>{
     setInterval(() =>{
         const memberCount = guild.memberCount;
         const channel = guild.channels.cache.get('YOUR_CHANNEL_ID');
-        channel.setName(`Total Members: ${memberCount.toLocaleString()}`);
-        console.log('Updating Member Count');
+        channel.setName(`Members: ${memberCount.toLocaleString()}`);
+        console.log('Updating Member Count'); // <--- Delete this line, if you dont want your console to get spammed.
     }, 5000);
 }
